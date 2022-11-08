3dfec4d67db321ed214c1204b5ed2fbb737c41b6
omkar <Omkar Joshi>
2022-11-08 16:53:15 +0530

    this is final for deploy

--- xmeme-backend/Routes/upload.js
+++ xmeme-backend/Routes/upload.js
@@ -1,4 +1,4 @@
-const upload = require("../middleware/upload");
+const upload = require("../Middleware/upload");
 const express = require("express");
 const Grid = require("gridfs-stream");
 const mongoose = require("mongoose");
